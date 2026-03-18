e9e05e79f2cd658b52cb84a7028109f4a75b66fd
Martha Lopez <marthalopezaguirre7@gmail.com>
2026-03-18 16:26:04 -0700

    Main page setup + particle maze
    Main css styling and p5 canvas ending done w particle maze done

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+
+let img;
+let safe;
+let x = 700;
+let y = 630;
+
+function preload(){
+  img = loadImage('kitty.png');
+}
 function setup() {
-  createCanvas(400, 400);
+  let canvas = createCanvas(1000, 800);
+  canvas.parent('maze-holder');
+  background('#3A232A')
+  image(img,x,y, 150,150);
+  safe = true;
 }
 
 function draw() {
-  background(220);
+ noStroke();
+ rect(700, 600, 20, 200); //verticle left
+ rect(700, 600, 150, 20);// horizontal
+ rect(830, 350, 20, 250);// verticle right
+ rect(700, 330, 150, 20);// horizontal
+ rect(700, 330, 20, 100);// verticle down
+ rect(830, 0, 20, 130); // verticle top 
+ rect(400, 110, 300, 20); // h
+ rect(400, 110, 20, 140);//v 
+ rect(190, 270, 300, 20) // h
 }
+
