b24bec63530ec38b0248d1575ca548cfbe174338
Martha Lopez <marthalopezaguirre7@gmail.com>
2026-03-19 17:16:46 -0700

    finished maze + arrow key movement
    Completed maze (drawn) and the arrow keys move mr midnight however  still have to code function for checking if character has hit a wall and when they have hit the star

--- sketch.js
+++ sketch.js
@@ -10,12 +10,14 @@ function preload(){
 function setup() {
   let canvas = createCanvas(1000, 800);
   canvas.parent('maze-holder');
-  background('#3A232A')
+  background('#3A232A');
   image(img,x,y, 150,150);
   safe = true;
 }
 
 function draw() {
+  background('#3A232A');
+  //maze
  noStroke();
  rect(700, 600, 20, 200); //verticle left
  rect(700, 600, 150, 20);// horizontal
@@ -24,7 +26,29 @@ function draw() {
  rect(700, 330, 20, 100);// verticle down
  rect(830, 0, 20, 130); // verticle top 
  rect(400, 110, 300, 20); // h
- rect(400, 110, 20, 140);//v 
- rect(190, 270, 300, 20) // h
+ rect(400, 110, 20, 160);//v 
+ rect(180, 270, 300, 20); // h
+ rect(0, 110, 200, 20); // h
+ rect(200, 270, 20, 160);// v
+ rect(180, 430, 300, 20); // h
+ rect(460, 440, 20, 160); //v
+ rect(0, 600,250,20 );
+ circle(30,30, 40); //end point
+
+ image(img,x,y, 150,150);
+}
+
+function keyPressed(){
+  if (keyCode === RIGHT_ARROW) {
+    x += 20;
+  } else if (keyCode === DOWN_ARROW) {
+   y+=20;
+  } else if (keyCode === LEFT_ARROW) {
+    x -= 20;
+  } else if (keyCode === UP_ARROW) {
+    y-=20;
+  }
+
+  
 }
 
